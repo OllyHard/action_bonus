81bcc77c98f7d1b85b802e88aff65f353abede35
OllyHard <pelmeni.95@mail.ru>
2020-09-18 22:39:38 +0700

    redaction 2.0

--- Bonus.java
+++ Bonus.java
@@ -2,20 +2,14 @@ package bonus;
 
 public class Bonus {
     public static void main(String[] args) {
-        boolean action = true;
         int currentBalance = 300;
         int transfer = 1200;
-        int percent;
-        if (action) {
-            percent = 1;
-        } else {
-            percent = 0;
+        int bonus = 0;
+        if (transfer > 1000) {
+            bonus = transfer * 1 / 100;
         }
-        int bonus = transfer * percent / 100;
-        int limit = 1000;
-        if (bonus > limit) {
-            bonus = limit;
-        }
+        int total = currentBalance + transfer + bonus;
         System.out.println(bonus);
+        System.out.println(total);
     }
 }
